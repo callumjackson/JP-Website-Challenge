2d8f8a5d65a3f344f34e7804aa01c92e39a9363e
Callum <j1228048@student.southstaffs.ac.uk>
2019-08-09 22:54:33 +0100

    added the mobile backgrounds and finaly got my java to work

--- js/own-js.js
+++ js/own-js.js
@@ -1,9 +1,1 @@
-var elem = document.querySelector('.sidenav');
- var instance = new M.Sidenav(elem);
-
- document.addEventListener('DOMContentLoaded', function() {
-   var elems = document.querySelectorAll('.scrollspy');
-  
-
-  var instance = M.ScrollSpy.getInstance(elem);
- });
+  M.AutoInit();
