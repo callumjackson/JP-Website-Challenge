010cd05fdc13d0cc7339386da3817bb9b489ad7f
Callum <j1228048@student.southstaffs.ac.uk>
2019-08-11 23:37:20 +0100

    final tweeks and adding footer

---
+++ HeadItems.php
@@ -0,0 +1,19 @@
+<?php
+$PageAddress = "http://websitechallenge.co.uk/index.html";
+$PageTitle = "Company Name";
+$Keywords = "Template-page, Website-challenge";
+$PageDisc = "A website created for Jones and Palmer";
+$FrontImgAddress = "http://websitechallenge.co.uk/assets/JP-logo/link-preview.png";
+$CreateTime = "2019-08-11T00:20:14+01:00";
+$EditTime = "2018-04-29T00:20:15+01:40";
+$LogoImg = "http://websitechallenge.co.uk/assets/JP-logo/logo-small-black-icon.png";
+
+/*If you want analytics setup*/
+$Analytics = "1";
+
+
+/*Page is to be restricted 1 = Yes / 0 = No *What rank from
+If index is restricted it will show a 403 page*/
+$Lockout = '1*4';
+
+?>
--- index.php
+++ index.php
@@ -1,19 +1,69 @@
+<?php
+include 'HeadItems.php' ;
+ ?>
 <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  	  <title><?php echo $PageTitle ?></title>
+      <meta name="keywords" content="<?php echo $Keywords ?>">
+
+
+      	<meta name="description" content="<?php echo $PageDisc ?>"/>
+      	<!-- Schema.org markup for Google+ -->
+      	<meta itemprop="name" content="<?php echo $PageTitle ?>"/>
+      	<meta itemprop="description" content="<?php echo $PageDisc ?>"/>
+      	<meta itemprop="image" content="<?php echo $FrontImgAddress ?>"/>
+      	<!-- Twitter Card data -->
+      	<meta name="twitter:card" content="summary_large_image"/>
+      	<meta name="twitter:title" content="<?php echo $PageTitle ?>"/>
+      	<meta name="twitter:description" content="<?php echo $PageDisc ?>"/>
+      	<!-- Twitter summary card with large image must be at least 280x150px -->
+      	<meta name="twitter:image:src" content="<?php echo $FrontImgAddress ?>"/>
+      	<!-- Open Graph data -->
+      	<meta property="og:title" content="<?php echo $PageTitle ?>"/>
+      	<meta property="og:type" content="website"/>
+      	<meta property="og:url" content="<?php echo $PageAddress ?>"/>
+      	<meta property="og:image" content="<?php echo $FrontImgAddress ?>"/>
+      	<meta property="og:description" content="<?php echo $PageDisc ?>"/>
+      	<meta property="og:site_name" content="<?php echo $PageTitle ?>"/>
+      	<meta property="article:published_time" content="<?php echo $CreateTime ?>"/>
+      	<meta property="article:modified_time" content="<?php echo $EditTime ?>"/>
+      	<meta property="article:section" content="Article Section"/>
+      	<meta property="article:tag" content="Article Tag"/>
+      	<!-- DEVICE ICONS -->
+      	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+      	<link rel="icon" href="<?php echo $LogoImg ?>">
+      	<link rel="shortcut icon" href="<?php echo $LogoImg ?>" />
+      	<link rel="apple-touch-icon" href="<?php echo $LogoImg ?>"/>
+      	<link rel="apple-touch-icon-precomposed" href="<?php echo $LogoImg ?>"/>
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize" media="screen,projection">
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
-      <link type="text/css" rel="stylesheet" href="css/own-css.css" media="screen,projection">
+      <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen">
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen"/>
+      <link type="text/css" rel="stylesheet" href="css/own-css.css" media="screen">
       <link href="https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap" rel="stylesheet">
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/
     </head>
     <body>
-      <header> <!-- area containing the nav bar -->
+      <section>
+        <div  class="scrollspy" id="main">
+          <!-- top of the page -->
+        </div>
+
+<div class="fixed-action-btn">
+  <a href="#main" class="btn-floating btn-large red">
+    <i class="large material-icons">arrow_upward</i>
+  </a>
+</div>
+      </section>
+      <header>
+
+
+
+
+        <!-- area containing the nav bar -->
         <div class="navbar-fixed">
 
         <nav>
@@ -23,13 +73,10 @@
 
     <div class="logo-wrapper">
       <a href="" class="brand-logo"><img class="JP-logo" src="assets/JP-logo/logo-small-white.svg" alt="JP logo"></a>
-      <a href=""class="title-text">Company Name</a>
-    </div>
-    <div class="mobile-view">
-      <a href=""class="brand-logo center">Company Name</a>
+      <a href=""class="title-text hide-on-med-and-down">Company Name</a>
     </div>
     <ul class="right hide-on-med-and-down nav-links">
-      <li><a href="index.html">Home</a></li>
+      <li><a href="#main">Home</a></li>
       <li><a href="#section1">Section 1</a></li>
       <li><a href="#section2">Section 2</a></li>
     </ul>
@@ -46,10 +93,10 @@
   </div>
       </header>
       <main>
-        <section class="section" id="main">
+        <section class="section">
           <div class="home-content">
             <div class="container">
-              <h3 class="center">Company Name</h3>
+              <h1 class="title-page">Company Name</h1>
               <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra, turpis at facilisis
                  aliquet, augue ligula cursus nisl, a commodo sapien diam pretium ex. Nullam viverra sem sed
                  magna posuere, et laoreet quam ultricies.</h5>
@@ -67,7 +114,7 @@
           </div>
       </section>
       <div class="parallax-container">
-        <div class="parallax"><img src="assets/homepage/para1.jpg"></div>
+        <div class="parallax"><img src="assets/homepage/para1.jpg" alt="orange mountain"></div>
       </div>
       <section class="">
         <div class="section1">
@@ -126,7 +173,7 @@
         </div>
       </section>
     <div class="parallax-container">
-      <div class="parallax"><img src="assets/homepage/para2.jpg"></div>
+      <div class="parallax"><img src="assets/homepage/para2.jpg" alt="purple mountian"></div>
     </div>
 <section>
   <div class="section2">
@@ -172,7 +219,28 @@
 </section>
 
       </main>
-
+      <footer class="page-footer">
+                <div class="container">
+                  <div class="row">
+                    <div class="col l6 s12">
+                      <img class="footer-logo" src="assets\JP-logo\logo-small-black.svg" alt="full JP logo">
+                      </div>
+                    <div class="col l4 offset-l2 s12">
+                      <ul class="footer-list">
+                        <li><a class="grey-text text-lighten-3" href="#!">accessibility</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Sitemap</a></li>
+                      </ul>
+                      <img class="footer-img" src="assets\JP-logo\logo-full-black.svg" alt="full JP logo">
+                    </div>
+                  </div>
+                </div>
+                <div class="footer-copyright">
+                  <div class="container">
+                  © 2019 Company Name - by Callum Jackson
+                  </div>
+                </div>
+              </footer>
 
 
 
